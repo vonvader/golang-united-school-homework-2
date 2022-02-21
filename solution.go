d7ca7d9c80424f7b90f9e8d270388f2046e10b55
Alexander Oplesnin <aoplesnin@Alexs-MBP.local>
2022-02-20 20:32:58 -0800

    Fixed circle calculations

--- solution.go
+++ solution.go
@@ -19,7 +19,7 @@ const (
 
 func CalcSquare(sideLen float64, sidesNum int) float64 {
 	if sidesNum == 0 {
-		return math.Pi * sideLen
+		return math.Pi * sideLen * sideLen
 	} else if sidesNum == 3 {
 		return sideLen * sideLen * math.Sqrt(3) / 4
 	} else if sidesNum == 4 {
